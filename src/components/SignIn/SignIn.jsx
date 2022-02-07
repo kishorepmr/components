e591a2b38abcc23c424d8416f746e85aee22c993
Kishore Ponmuthuramalingam <kponmuth@cisco.com>
2022-02-07 18:05:09 +0530

    feat(SignIn): open browser session and handle window closed

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Button} from '../generic';
 
 /**
  * SignIn component performs OAuth 2.0 authentication.
  *
+ * @param {object} props Data passed to the component
+ * @param {string} props.authUrl Authorization url
+ * @param {string} props.clientID Client ID of the app
+ * @param {string} props.redirectUri Redirect Url registered to capture the code
+ * @param {string} props.scope Scope required for the app
+ * @param {Function} props.signInResponse Function called on successfull sign in
+ * @param {Function} props.getAccessToken Function called to fetch access token from backend server
  * @returns {object} JSX of the component
  */
-export default function SignIn() {
+export default function SignIn({
+  authUrl,
+  clientID,
+  redirectUri,
+  scope,
+  signInResponse,
+  getAccessToken,
+}) {
+  const openAuthUrl = () => {
+    const arr = new Uint8Array(4);
+    const state = window.crypto.getRandomValues(arr);
+    const fullAuthUrl = `${authUrl}?client_id=${clientID}&response_type=code&redirect_uri=${encodeURI(redirectUri)}${scope !== '' ? `&scope=${encodeURI(scope)}` : ''}&state=${state}`;
+
+    const newWindow = window.open(fullAuthUrl, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=400,height=700');
+    const timer = setInterval(() => {
+      if (newWindow.closed) {
+        clearInterval(timer);
+        const accessToken = getAccessToken();
+
+        signInResponse(clientID, accessToken || Error('No access token was returned'));
+      }
+    }, 500);
+  };
+
   return (
     <div className="sign-in-wrapper">
       <Button
         type="join"
         size={40}
-        onClick={() => {}}
+        onClick={openAuthUrl}
       >
         Sign In
       </Button>
@@ -21,7 +52,16 @@ export default function SignIn() {
 }
 
 SignIn.propTypes = {
+  authUrl: PropTypes.string.isRequired,
+  clientID: PropTypes.string.isRequired,
+  redirectUri: PropTypes.string.isRequired,
+  scope: PropTypes.string,
+  signInResponse: PropTypes.func,
+  getAccessToken: PropTypes.func,
 };
 
 SignIn.defaultProps = {
+  scope: '',
+  signInResponse: () => {},
+  getAccessToken: () => {},
 };
